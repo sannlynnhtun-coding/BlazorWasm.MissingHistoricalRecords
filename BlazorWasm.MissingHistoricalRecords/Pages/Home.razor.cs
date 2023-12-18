fa1209cc239ac32bcee2285595d6071230808d32
Myat Phone Thant <133953352+MClox@users.noreply.github.com>
2023-12-18 23:57:53 +0630

    [InProgress] UI adding

---
+++ BlazorWasm.MissingHistoricalRecords/Pages/Home.razor.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.JSInterop;
+
+namespace BlazorWasm.MissingHistoricalRecords.Pages;
+
+public partial class Home
+{
+    protected override async Task OnAfterRenderAsync(bool firstRender)
+    {
+        if (firstRender)
+        {
+            await Task.Delay(500); 
+            await JsRuntime.InvokeVoidAsync("scrollTop");
+            await JsRuntime.InvokeVoidAsync("loadJs", "themes/js/main.js");
+            StateHasChanged();
+        }
+    }
+}
